2fa940ec652ad1e440e88e713d539ee07fe5f33d
Marijana Kolarov <marijanakolarov01@gmail.com>
2026-04-25 11:46:52 +0200

    Write Funkcija

--- dCom-radna-verzija/dCom/Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
+++ dCom-radna-verzija/dCom/Modbus/ModbusFunctions/WriteSingleCoilFunction.cs
@@ -24,15 +24,43 @@ namespace Modbus.ModbusFunctions
         /// <inheritdoc />
         public override byte[] PackRequest()
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            ModbusWriteCommandParameters p = (ModbusWriteCommandParameters)CommandParameters;
+
+            byte[] request = new byte[6];
+
+            //Function code 05
+            request[0] = 5;
+
+            //Addr
+            request[1] = (byte)(p.OutputAddress >> 8);
+            request[2] = (byte)(p.OutputAddress & 0xFF);
+
+            //Value
+            if (p.Value == 1)
+            {
+                request[3] = 0xFF;
+                request[4] = 0x00;
+            }
+            else 
+            {
+                request[3] = 0x00;
+                request[4] = 0x00;
+            }
+            return request;
         }
 
         /// <inheritdoc />
         public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
         {
-            //TO DO: IMPLEMENT
-            throw new NotImplementedException();
+            Dictionary<Tuple<PointType, ushort>, ushort> result =new Dictionary<Tuple<PointType, ushort>, ushort>();
+
+            ushort address = (ushort)((response[1] << 8) | response[2]);
+
+            ushort value = (response[3] == 0xFF) ? (ushort)1 : (ushort)0;
+
+            result.Add(new Tuple<PointType, ushort>(PointType.DIGITAL_OUTPUT, address), value);
+        
+            return result;
         }
     }
 }
